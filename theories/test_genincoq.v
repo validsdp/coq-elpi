4b8fedfd6c00437573a99b0e444455bad76b13aa
lucas8 <luc.linux@mailoo.org>
2017-07-31 19:08:30 +0200

    implement generics instance derivation

--- theories/test_genincoq.v
+++ theories/test_genincoq.v
@@ -4,24 +4,28 @@ Elpi Init "./" "./elpi/".
 Elpi Accumulate File "pervasives.elpi".
 
 Module Generics.
-  Record class (T : Type) := Class { rep : Type;
-                                     f   : T -> rep;
-                                     g   : rep -> option T;
-                                     inj : forall (x : T), g (f x) = Some x
+  Record class (T : Type) := Class { rep  : Type;
+                                     f    : T -> rep;
+                                     g    : rep -> T;
+                                     inj  : forall (x : T), g (f x) = x;
+                                     surj : forall (x : rep), f (g x) = x
                                    }.
   Structure type := Pack { obj : Type; class_of : class obj }.
-  Arguments Class {T} rep f g inj.
+  Arguments Class {T} rep f g inj surj.
 
   Definition from (e : type) : obj e -> rep (obj e) (class_of e) :=
-    let 'Pack _ (Class _ f _ _) := e in f.
-  Definition to (e : type) : rep (obj e) (class_of e) -> option (obj e) :=
-    let 'Pack _ (Class _ _ g _) := e in g.
+    let 'Pack _ (Class _ f _ _ _) := e in f.
+  Definition to (e : type) : rep (obj e) (class_of e) -> (obj e) :=
+    let 'Pack _ (Class _ _ g _ _) := e in g.
   Arguments from {e} x : simpl never.
   Arguments to   {e} x : simpl never.
 
-  Definition to_from_id (e : type) : forall (x : obj e), to (from x) = Some x :=
-    let 'Pack _ (Class _ _ _ inj) := e in inj.
+  Definition to_from_id (e : type) : forall (x : obj e), to (from x) = x :=
+    let 'Pack _ (Class _ _ _ inj _) := e in inj.
+  Definition from_to_id (e : type) : forall (x : rep (obj e) (class_of e)), from (to x) = x :=
+    let 'Pack _ (Class _ _ _ _ surj) := e in surj.
   Arguments to_from_id {e} : simpl never.
+  Arguments from_to_id {e} : simpl never.
 End Generics.
 
 Inductive SumType (A B : Type) : Type :=
@@ -34,6 +38,22 @@ Inductive ProdType (A B : Type) : Type :=
 Inductive UnitType : Type :=
 | MkUnit.
 
+
+Inductive MList (A B : Type) :=
+| MCons : A -> B -> MList A B -> MList A B
+| MNil : MList A B.
+
+Inductive MTree (A : Type) :=
+| MNode : MList A (MTree (A * A)) -> MTree A.
+
+
 Elpi Accumulate File "genincoq.elpi".
-Elpi Run "make-rep-test.".
+Elpi Run "derive-generics ""list"".".
+Print list_generics.
+Elpi Run "derive-generics ""nat"".".
+Print nat_generics.
+Elpi Run "derive-generics ""MList"".".
+Print MList_generics.
+Elpi Run "derive-generics ""MTree"".".
+Print MTree_generics.
 
